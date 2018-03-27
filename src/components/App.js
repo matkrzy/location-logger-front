aefb350d5e2719201fabbff5dc9d2533b59844de
Mateusz Krzyżanowski <matkrzy94@gmail.com>
2018-03-27 17:44:15 +0200

    Revert "Add home component"

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,16 @@ import { routes } from './routes';
 import { LoginContainer } from './login/LoginContainer';
 import { RegisterContainer } from './register/RegisterContainer';
 import { ForgotPasswordContainer } from './forgot-password/ForgotPasswordContainer';
-import { HomeContainer } from './home/HomeContainer';
 
 export class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path={routes.home} component={HomeContainer} />
+          <Route exact path={routes.home} render={() => <div>home</div>} />
           <Route path={routes.login} component={LoginContainer} />
           <Route path={routes.register} component={RegisterContainer} />
-          <Route
-            path={routes.forgotPassword}
-            component={ForgotPasswordContainer}
-          />
+          <Route path={routes.forgotPassword} component={ForgotPasswordContainer} />
         </Switch>
       </div>
     );
